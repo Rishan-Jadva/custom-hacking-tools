e9785ae9f8d5cd7460daf72dd9afb22bb0d5e7a6
Rishan-Jadva <rishanjadva@gmail.com>
2025-05-15 21:22:20 +1000

    created tcp_scanner
    able to look for open ports

--- portscanner/scanner/tcp_scanner.py
+++ portscanner/scanner/tcp_scanner.py
@@ -1,1 +1,26 @@
 # TCP connect of SYN scan (socket or scapy)
+import socket
+
+def check_port(target, port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        result = s.connect_ex((target, port))
+        s.close()
+        if result == 0:
+            return "Open"
+        else:
+            return "Closed/Filtered"
+    except socket.timeout:
+        return "Filtered"
+    except Exception as e:
+        return f"Error: {e}"
+
+def scan_tcp_ports(target, ports):
+    results = {}
+
+    for port in ports:
+        status = check_port(target, port)
+        results[port] = status
+    
+    return results
--- portscanner/scripts/run_scan.py
+++ portscanner/scripts/run_scan.py
@@ -9,3 +9,9 @@ use response parser to interpret open/close/filtered fields
 use service detect for banner grabbing
 write results to reports/
 """
+from ..scanner import tcp_scanner
+
+target = "192.168.56.101"
+ports = [21, 22, 80, 9999, 4444]
+
+print(tcp_scanner.scan_tcp_ports(target, ports))
